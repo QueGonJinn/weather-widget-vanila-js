f310dc30d22a7e32783180a479ecf5fbf4cfc044
Yury Moliakow <molialow13@gmail.com>
2023-04-13 16:39:39 +0300

    feat: add ip search

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const options = {
 	maximumAge: 0,
 };
 
-const days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-const daysM = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
+const days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+const daysM = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
 
 const month = [
 	'Января',
@@ -66,29 +66,25 @@ class DayilyCard {
 		elem.classList.add('weather__week__days');
 
 		elem.innerHTML = `
-                        <div class="day__wrapper">
-                                <span class="day__name">${
-																	daysM[new Date(this.daily * 1000).getDay()]
-																}</span>
-                                <div class="currentday__temperature-wrapper">
-                                    <span class="day__temp">${
-																			Math.round(this.dayTemp * 10) / 10
-																		}</span>
-                                    <span class="day__separator">o</span>                                    
-                                </div>
-                                <div class="currentday__temperature-wrapper">
-                                    <span class="day__temp">${
-																			Math.round(this.nightTemp * 10) / 10
-																		}</span>
-                                    <span class="day__separator">o</span>                                    
-                                </div>
-                                <div class="day__img">
-                                    <img src=https://openweathermap.org/img/wn/${
-																			this.icon
-																		}@2x.png alt="day__icon" />
-                                </div>
-                        </div>
-                `;
+			<div class="day__wrapper">
+							<span class="day__name">${daysM[new Date(this.daily * 1000).getDay()]}</span>
+							<div class="currentday__temperature-wrapper">
+									<span class="day__temp">${
+										this.dayTemp > 0 ? `+${Math.round(this.dayTemp)}` : Math.round(this.dayTemp)
+									}</span>
+							</div>
+							<div class="currentday__temperature-wrapper">
+									<span class="day__temp">${
+										this.nightTemp > 0
+											? `+${Math.round(this.nightTemp)}`
+											: Math.round(this.nightTemp)
+									}</span>
+							</div>
+							<div class="day__img">
+									<img src=https://openweathermap.org/img/wn/${this.icon}@2x.png alt="day__icon" />
+							</div>
+			</div>
+    `;
 		this.parrent.append(elem);
 	}
 }
@@ -105,22 +101,24 @@ class HourlyCard {
 		elem.classList.add('hourly__items');
 
 		elem.innerHTML = `
-                        <div class="hourly__wrapper">
-                            <span class="hourly__name">${new Date(
-															this.daily * 1000,
-														).getHours()} ч</span>
-                                <div class="currentday__temperature-wrapper">
-                                    <span class="day__temp">${
-																			Math.round(this.dayTemp * 10) / 10
-																		}</span>
-                                    <span class="day__separator">o</span>                                    
-                                </div>
-                        </div>
-                `;
+			<div class="hourly__wrapper">
+					<div class="hourly__name">
+					<span class="hourly__item">${new Date(this.daily * 1000).getHours()}</span>
+					<span class="hourly__sep">ч</span>
+					</div>
+							<div class="currentday__temperature-wrapper">
+									<span class="day__temp">${
+										this.dayTemp > 0 ? `+${Math.round(this.dayTemp)}` : Math.round(this.dayTemp)
+									}</span>
+																										
+							</div>
+			</div>
+		`;
 		this.parrent.append(elem);
 	}
 }
 
+const checkLocation = document.getElementById('check__location');
 const currentDateDay = document.querySelector('.current__day');
 const currentCity = document.querySelector('.current__location');
 const currentTemp = document.querySelector('.current__temp');
@@ -133,10 +131,19 @@ const currentDirection = document.getElementById('current__wind_degree_param');
 const currentFeelsLike = document.querySelector('.current__feels__like');
 const preload = document.querySelector('.preload');
 
-function success(pos) {
-	const crd = pos.coords;
+const appFirst = async () => {
+	const cityIP = await fetch(
+		`https://api.geoapify.com/v1/ipinfo?lang=ru&apiKey=5f52dceecbd046de9e150000ef5a2f4e`,
+	);
+	const cityIpData = await cityIP.json();
 
-	const getWeatherDate = async (latitude = 53.89, longitude = 27.56) => {
+	const city = await fetch(
+		`https://api.geoapify.com/v1/geocode/reverse?lat=${cityIpData.location.latitude}&lon=${cityIpData.location.longitude}&lang=ru&apiKey=5f52dceecbd046de9e150000ef5a2f4e`,
+	);
+
+	const cityData = await city.json();
+
+	const getWeatherDate = async (latitude, longitude) => {
 		try {
 			const response = await fetch(
 				`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely&appid=11329a68c70484de9078f50717a0d4b7&units=metric&lang=ru`,
@@ -148,113 +155,186 @@ function success(pos) {
 		}
 	};
 
-	const app = async () => {
-		const weather = await getWeatherDate(crd.latitude, crd.longitude);
+	const weather = await getWeatherDate(cityIpData.location.latitude, cityIpData.location.longitude);
 
-		const city = await fetch(
-			`https://api.geoapify.com/v1/geocode/reverse?lat=${crd.latitude}&lon=${crd.longitude}&lang=ru&apiKey=5f52dceecbd046de9e150000ef5a2f4e`,
-		);
+	currentCity.innerHTML = cityData.features[0].properties.county;
+	currentDateDay.innerHTML = ` ${days[new Date(weather.current.dt * 1000).getDay()]}`;
+	currentTemp.innerHTML =
+		weather.current.temp > 0
+			? `+${Math.round(weather.current.temp)}`
+			: Math.round(weather.current.temp);
+	currentIcon.setAttribute(
+		'src',
+		`https://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png`,
+	);
+	currentDayInfo.innerHTML = weather.current.weather[0].description;
 
-		const cityData = await city.json();
+	currentPressure.innerHTML = ` ${weather.current.pressure} мм.рт.ст`;
+	currentHumidity.innerHTML = ` ${weather.current.humidity} %`;
+	currentWind.innerHTML = ` ${Math.round(weather.current.wind_speed * 10) / 10} м/с`;
+	currentDirection.innerHTML = directionOfWind(weather.current.wind_deg);
+	currentFeelsLike.innerHTML =
+		weather.current.feels_like > 0
+			? `+${Math.round(weather.current.feels_like)}`
+			: Math.round(weather.current.feels_like);
+	if (currentIcon.getAttribute('src') !== 0) {
+		preload.setAttribute('style', 'visibility: hidden');
+	}
 
-		currentCity.innerHTML = cityData.features[0].properties.city;
-		currentDateDay.innerHTML = ` ${days[new Date(weather.current.dt * 1000).getDay() - 1]}`;
-		currentTemp.innerHTML = Math.round(weather.current.temp * 10) / 10;
-		currentIcon.setAttribute(
-			'src',
-			`https://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png`,
-		);
-		currentDayInfo.innerHTML = weather.current.weather[0].description;
-
-		currentPressure.innerHTML = ` ${weather.current.pressure} мм.рт.ст`;
-		currentHumidity.innerHTML = ` ${weather.current.humidity} %`;
-		currentWind.innerHTML = ` ${weather.current.wind_speed} м/с`;
-		currentDirection.innerHTML = directionOfWind(weather.current.wind_deg);
-		currentFeelsLike.innerHTML = Math.round(weather.current.feels_like * 10) / 10;
-
-		if (currentIcon.getAttribute('src') !== 0) {
-			preload.setAttribute('style', 'visibility: hidden');
+	weather.daily.forEach((e, i) => {
+		if (i < 6 && i !== 0) {
+			new DayilyCard(e, '.weather__week').render();
 		}
+	});
 
-		weather.daily.forEach((e, i) => {
-			if (i < 6) {
-				new DayilyCard(e, '.weather__week').render();
-			}
-		});
-
-		weather.hourly.forEach((e, i) => {
-			if (i < 15 && i !== 0 && i % 2 === 0) {
-				new HourlyCard(e, '.weather__hourly').render();
-			}
-		});
-
-		console.log(cityData);
-		console.log(weather);
-	};
-
-	app();
-}
-
-function error(err) {
-	const latitude = 53.89;
-	const longitude = 27.56;
-	console.warn(`ERROR(${err.code}): ${err.message}`);
-
-	const getWeatherDate = async (latitude = 53.89, longitude = 27.56) => {
-		try {
-			const response = await fetch(
-				`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely&appid=11329a68c70484de9078f50717a0d4b7&units=metric&lang=ru`,
-			).then((res) => res.json());
-
-			return response;
-		} catch (error) {
-			console.error(error);
+	weather.hourly.forEach((e, i) => {
+		if (i < 15 && i !== 0 && i % 2 === 0) {
+			new HourlyCard(e, '.weather__hourly').render();
 		}
-	};
+	});
 
-	const app = async () => {
-		const weather = await getWeatherDate();
+	console.log(cityData);
+	console.log(weather);
+	console.log(cityIpData);
+};
 
-		const city = await fetch(
-			`https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&lang=ru&apiKey=5f52dceecbd046de9e150000ef5a2f4e`,
-		);
+appFirst();
 
-		const cityData = await city.json();
+checkLocation.addEventListener('click', () => {
+	const wetherHoyrly = document.querySelector('.weather__hourly');
+	const weatherWeek = document.querySelector('.weather__week');
+	wetherHoyrly.innerHTML = '';
+	weatherWeek.innerHTML = '';
 
-		currentCity.innerHTML = cityData.features[0].properties.city;
-		currentDateDay.innerHTML = `, ${days[new Date(weather.current.dt * 1000).getDay() - 1]}`;
-		currentTemp.innerHTML = Math.round(weather.current.temp * 10) / 10;
-		currentIcon.setAttribute(
-			'src',
-			`https://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png`,
-		);
-		currentDayInfo.innerHTML = weather.current.weather[0].description;
-		currentPressure.innerHTML = `${weather.current.pressure} мм.рт.ст`;
-		currentHumidity.innerHTML = `${weather.current.humidity} %`;
-		currentWind.innerHTML = `${weather.current.wind_speed} м/с`;
-		currentDirection.innerHTML = directionOfWind(weather.current.wind_deg);
-		currentFeelsLike.innerHTML = Math.round(weather.current.feels_like * 10) / 10;
+	function success(pos) {
+		const crd = pos.coords;
 
-		if (currentIcon.getAttribute('src') !== 0) {
-			preload.setAttribute('style', 'visibility: hidden');
-		}
+		const getWeatherDate = async (latitude = 53.89, longitude = 27.56) => {
+			try {
+				const response = await fetch(
+					`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely&appid=11329a68c70484de9078f50717a0d4b7&units=metric&lang=ru`,
+				).then((res) => res.json());
 
-		weather.daily.forEach((e, i) => {
-			if (i < 6) {
-				new DayilyCard(e, '.weather__week').render();
+				return response;
+			} catch (error) {
+				console.error(error);
 			}
-		});
+		};
 
-		weather.hourly.forEach((e, i) => {
-			if (i < 15 && i !== 0 && i % 2 === 0) {
-				new HourlyCard(e, '.weather__hourly').render();
+		const app = async () => {
+			const weather = await getWeatherDate(crd.latitude, crd.longitude);
+
+			const city = await fetch(
+				`https://api.geoapify.com/v1/geocode/reverse?lat=${crd.latitude}&lon=${crd.longitude}&lang=ru&apiKey=5f52dceecbd046de9e150000ef5a2f4e`,
+			);
+
+			const cityData = await city.json();
+
+			currentCity.innerHTML = cityData.features[0].properties.city;
+			currentDateDay.innerHTML = ` ${days[new Date(weather.current.dt * 1000).getDay()]}`;
+			currentTemp.innerHTML =
+				weather.current.temp > 0
+					? `+${Math.round(weather.current.temp)}`
+					: Math.round(weather.current.temp);
+			currentIcon.setAttribute(
+				'src',
+				`https://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png`,
+			);
+			currentDayInfo.innerHTML = weather.current.weather[0].description;
+
+			currentPressure.innerHTML = ` ${weather.current.pressure} мм.рт.ст`;
+			currentHumidity.innerHTML = ` ${weather.current.humidity} %`;
+			currentWind.innerHTML = ` ${Math.round(weather.current.wind_speed * 10) / 10} м/с`;
+			currentDirection.innerHTML = directionOfWind(weather.current.wind_deg);
+			currentFeelsLike.innerHTML =
+				weather.current.feels_like > 0
+					? `+${Math.round(weather.current.feels_like)}`
+					: Math.round(weather.current.feels_like);
+			if (currentIcon.getAttribute('src') !== 0) {
+				preload.setAttribute('style', 'visibility: hidden');
 			}
-		});
-		console.log(cityData);
-		console.log(weather);
-	};
 
-	app();
-}
+			weather.daily.forEach((e, i) => {
+				if (i < 6 && i !== 0) {
+					new DayilyCard(e, '.weather__week').render();
+				}
+			});
 
-navigator.geolocation.getCurrentPosition(success, error, options);
+			weather.hourly.forEach((e, i) => {
+				if (i < 15 && i !== 0 && i % 2 === 0) {
+					new HourlyCard(e, '.weather__hourly').render();
+				}
+			});
+		};
+
+		app();
+	}
+
+	function error(err) {
+		const latitude = 53.89;
+		const longitude = 27.56;
+		console.warn(`ERROR(${err.code}): ${err.message}`);
+
+		const getWeatherDate = async (latitude = 53.89, longitude = 27.56) => {
+			try {
+				const response = await fetch(
+					`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely&appid=11329a68c70484de9078f50717a0d4b7&units=metric&lang=ru`,
+				).then((res) => res.json());
+
+				return response;
+			} catch (error) {
+				console.error(error);
+			}
+		};
+
+		const app = async () => {
+			const weather = await getWeatherDate();
+
+			const city = await fetch(
+				`https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&lang=ru&apiKey=5f52dceecbd046de9e150000ef5a2f4e`,
+			);
+
+			const cityData = await city.json();
+
+			currentCity.innerHTML = cityData.features[0].properties.city;
+			currentDateDay.innerHTML = `${days[new Date(weather.current.dt * 1000).getDay()]}`;
+			currentTemp.innerHTML =
+				weather.current.temp > 0
+					? `+${Math.round(weather.current.temp)}`
+					: Math.round(weather.current.temp);
+			currentIcon.setAttribute(
+				'src',
+				`https://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png`,
+			);
+			currentDayInfo.innerHTML = weather.current.weather[0].description;
+			currentPressure.innerHTML = `${weather.current.pressure} мм.рт.ст`;
+			currentHumidity.innerHTML = `${weather.current.humidity} %`;
+			currentWind.innerHTML = ` ${Math.round(weather.current.wind_speed * 10) / 10} м/с`;
+			currentDirection.innerHTML = directionOfWind(weather.current.wind_deg);
+			currentFeelsLike.innerHTML =
+				weather.current.feels_like > 0
+					? `+${Math.round(weather.current.feels_like)}`
+					: Math.round(weather.current.feels_like);
+
+			if (currentIcon.getAttribute('src') !== 0) {
+				preload.setAttribute('style', 'visibility: hidden');
+			}
+
+			weather.daily.forEach((e, i) => {
+				if (i < 6 && i !== 0) {
+					new DayilyCard(e, '.weather__week').render();
+				}
+			});
+
+			weather.hourly.forEach((e, i) => {
+				if (i < 15 && i !== 0 && i % 2 === 0) {
+					new HourlyCard(e, '.weather__hourly').render();
+				}
+			});
+		};
+
+		app();
+	}
+
+	navigator.geolocation.getCurrentPosition(success, error, options);
+});
