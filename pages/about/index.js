4680f9b53491a3ff312f6a1d02e7e908d31445b3
Yury Moliakow <molialow13@gmail.com>
2023-04-15 11:00:37 +0300

    feat: update style

--- pages/about/index.js
+++ pages/about/index.js
@@ -71,70 +71,97 @@ class DayilyCard {
 		elem.classList.add('wrapper__day');
 
 		elem.innerHTML = `
-			
-					<div class="temp__daytime">
-						<div class="day__wrapper">
-							<span class="day__name">${days[new Date(this.daily * 1000).getDay()]}</span>
-							<span class="day__number">${new Date(this.daily * 1000).getDate()}</span>
-							<span class="day__month">${month[new Date(this.daily * 1000).getMonth()]}</span>
-						</div>
-						<div class="daytime_wrapper">
-							<div class="current__temperature">
-								<div class="currentday__temperature-wrapper">
-									<span class="day__temp">${Math.floor(this.dayTemp * 10) / 10}</span>
-									<span class="day__separator">o</span>
-									<span class="day__units">C</span>
-								</div>
-								<div class="currentday__temperature-wrapper">
-									<span class="day__temp">${Math.floor(this.nightTemp * 10) / 10}</span>
-									<span class="day__separator">o</span>
-									<span class="day__units">C</span>
-								</div>
-							</div>
-							<div class="day__img">
-								<img src=https://openweathermap.org/img/wn/${this.icon}@2x.png alt="day__icon" />
-							</div>
-						</div>
-						<div class="day__inform__weather">Пасмурно</div>
-					</div>
-					<div class="day__additions">
-						<div class="additions__item">
-							<div id="daytime__clouds" class="item__param">Облачнось:</div>
-							<div id="daytime__clouds__param" class="item__meaning">${this.clouds} %</div>
-						</div>
-						<div class="additions__item">
-							<div id="daytime__humidity" class="item__param">Влажность:</div>
-							<div id="daytime__humidity__param" class="item__meaning">${this.humidity} %</div>
-						</div>
-						<div class="additions__item">
-							<div id="daytime__wind" class="item__param">Ветер:</div>
-							<div id="daytime__wind__param" class="item__meaning">${this.wind} м/с</div>
-						</div>
-						<div class="additions__item">
-							<div id="daytime__wind__degree" class="item__param">Направление:</div>
-							<div id="daytime__wind__degree__param" class="item__meaning">${directionOfWind(
-								this.direction,
-							)}</div>
-						</div>
-						<div class="additions__item">
-							<div id="daytime__pressure" class="item__param">Давление:</div>
-							<div id="daytime__pressure__param" class="item__meaning">${this.pressure} мм.рт.ст</div>
-						</div>
-						<div class="additions__item">
-							<div id="daytime__sunrise" class="item__param">Восход:</div>
-							<div id="daytime__sunrise__param" class="item__meaning">${new Date(
-								this.sunrise * 1000,
-							).getHours()}:${new Date(this.sunrise * 1000).getMinutes()} </div>
-						</div>
-						<div class="additions__item">
-							<div id="daytime__sunset" class="item__param">Закат:</div>
-							<div id="daytime__sunset__param" class="item__meaning">${new Date(this.sunset * 1000).getHours()}:${
+			<div class="temp__daytime">
+                            <div class="day__wrapper">
+                                    <span class="day__name">${
+																			days[new Date(this.daily * 1000).getDay()]
+																		}</span>
+                                    <span class="day__number">${new Date(
+																			this.daily * 1000,
+																		).getDate()}</span>
+                                    <span class="day__month">${
+																			month[new Date(this.daily * 1000).getMonth()]
+																		}</span>
+                            </div>
+                            <div class="daytime_wrapper">
+                                    <div class="current__temperature">
+                                            <div class="currentday__temperature-wrapper">
+                                                    <span class="day__time">День: </span>
+                                                    <span class="day__temp">${
+																											this.dayTemp > 0
+																												? `+${Math.round(this.dayTemp)}`
+																												: Math.round(this.dayTemp)
+																										}</span>
+                                                    <span class="day__separator">o</span>
+                                                    <span class="day__units">C</span>
+                                            </div>
+                                            <div class="currentday__temperature-wrapper">
+                                                    <span class="day__time">Ночь: </span>
+                                                    <span class="day__temp">${
+																											this.nightTemp > 0
+																												? `+${Math.round(this.nightTemp)}`
+																												: Math.round(this.nightTemp)
+																										}</span>
+                                                    <span class="day__separator">o</span>
+                                                    <span class="day__units">C</span>
+                                            </div>
+                                    </div>
+                                    <div class="day__img">
+                                            <img src=https://openweathermap.org/img/wn/${
+																							this.icon
+																						}@2x.png alt="day__icon" />
+                                    </div>
+                            </div>
+                            <div class="day__inform__weather">${this.dayInfo}</div>
+                    </div>
+                    <div class="day__additions">
+                            <div class="additions__item">
+                                    <div id="daytime__clouds" class="item__param">Облачность:</div>
+                                    <div id="daytime__clouds__param" class="item__meaning">${
+																			this.clouds
+																		} %</div>
+                            </div>
+                            <div class="additions__item">
+                                    <div id="daytime__humidity" class="item__param">Влажность:</div>
+                                    <div id="daytime__humidity__param" class="item__meaning">${
+																			this.humidity
+																		} %</div>
+                            </div>
+                            <div class="additions__item">
+                                    <div id="daytime__wind" class="item__param">Ветер:</div>
+                                    <div id="daytime__wind__param" class="item__meaning">${
+																			Math.round(this.wind * 10) / 10
+																		} м/с</div>
+                            </div>
+                            <div class="additions__item">
+                                    <div id="daytime__wind__degree" class="item__param">Направление:</div>
+                                    <div id="daytime__wind__degree__param" class="item__meaning">${directionOfWind(
+																			this.direction,
+																		)}</div>
+                            </div>
+                            <div class="additions__item">
+                                    <div id="daytime__pressure" class="item__param">Давление:</div>
+                                    <div id="daytime__pressure__param" class="item__meaning">${
+																			this.pressure
+																		} мм.рт.ст</div>
+                            </div>
+                            <div class="additions__item add_sunrise">
+                                    <div id="daytime__sunrise" class="item__param">Восход - Закат: </div>
+                                    <div id="daytime__sunrise__param" class="item__meaning">
+                                            ${new Date(this.sunrise * 1000).getHours()}:${new Date(
+			this.sunrise * 1000,
+		).getMinutes()}
+                    </div>
+                    <span> - </span>
+                    <div id="daytime__sunset__param" class="item__meaning">
+                        ${new Date(this.sunset * 1000).getHours()}:${
 			new Date(this.sunset * 1000).getMinutes() < 10
 				? `0${new Date(this.sunset * 1000).getMinutes()}`
 				: new Date(this.sunset * 1000).getMinutes()
-		}</div>
-						</div>
-					</div>
+		}
+                    </div>
+                </div>
+                </div> 
 				
 		`;
 
@@ -143,11 +170,22 @@ class DayilyCard {
 }
 
 const currentCity = document.querySelector('.current__location');
+const checkLocation = document.getElementById('check__location');
+const weatherWeek = document.querySelector('.weather__week');
 
-function success(pos) {
-	const crd = pos.coords;
+const appFirst = async () => {
+	const cityIP = await fetch(
+		`https://api.geoapify.com/v1/ipinfo?lang=ru&apiKey=5f52dceecbd046de9e150000ef5a2f4e`,
+	);
+	const cityIpData = await cityIP.json();
 
-	const getWeatherDate = async (latitude = 53.89, longitude = 27.56) => {
+	const city = await fetch(
+		`https://api.geoapify.com/v1/geocode/reverse?lat=${cityIpData.location.latitude}&lon=${cityIpData.location.longitude}&lang=ru&apiKey=5f52dceecbd046de9e150000ef5a2f4e`,
+	);
+
+	const cityData = await city.json();
+
+	const getWeatherDate = async (latitude, longitude) => {
 		try {
 			const response = await fetch(
 				`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely&appid=11329a68c70484de9078f50717a0d4b7&units=metric&lang=ru`,
@@ -159,69 +197,97 @@ function success(pos) {
 		}
 	};
 
-	const app = async () => {
-		const weather = await getWeatherDate(crd.latitude, crd.longitude);
+	const weather = await getWeatherDate(cityIpData.location.latitude, cityIpData.location.longitude);
 
-		const city = await fetch(
-			`https://api.geoapify.com/v1/geocode/reverse?lat=${crd.latitude}&lon=${crd.longitude}&lang=ru&apiKey=5f52dceecbd046de9e150000ef5a2f4e`,
-		);
-
-		const cityData = await city.json();
-
-		currentCity.innerHTML = cityData.features[0].properties.city;
-
-		weather.daily.forEach((e, i) => {
-			if (i < 6 && i !== 0) {
-				new DayilyCard(e, '.weather__week').render();
-			}
-		});
-
-		console.log(cityData);
-		console.log(weather);
-	};
-
-	app();
-}
-
-function error(err) {
-	const latitude = 53.89;
-	const longitude = 27.56;
-	console.warn(`ERROR(${err.code}): ${err.message}`);
-
-	const getWeatherDate = async (latitude = 53.89, longitude = 27.56) => {
-		try {
-			const response = await fetch(
-				`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely&appid=11329a68c70484de9078f50717a0d4b7&units=metric&lang=ru`,
-			).then((res) => res.json());
-
-			return response;
-		} catch (error) {
-			console.error(error);
+	currentCity.innerHTML = cityData.features[0].properties.county;
+	weather.daily.forEach((e, i) => {
+		if (i < 6 && i !== 0) {
+			new DayilyCard(e, '.weather__week').render();
 		}
-	};
+	});
 
-	const app = async () => {
-		const weather = await getWeatherDate();
+	console.log(cityData);
+	console.log(weather);
+	console.log(cityIpData);
+};
 
-		const city = await fetch(
-			`https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&lang=ru&apiKey=5f52dceecbd046de9e150000ef5a2f4e`,
-		);
+appFirst();
 
-		const cityData = await city.json();
+checkLocation.addEventListener('click', () => {
+	weatherWeek.innerHTML = '';
+	function success(pos) {
+		const crd = pos.coords;
 
-		currentCity.innerHTML = cityData.features[0].properties.city;
+		const getWeatherDate = async (latitude = 53.89, longitude = 27.56) => {
+			try {
+				const response = await fetch(
+					`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely&appid=11329a68c70484de9078f50717a0d4b7&units=metric&lang=ru`,
+				).then((res) => res.json());
 
-		weather.daily.forEach((e, i) => {
-			if (i < 6 && i !== 0) {
-				new DayilyCard(e, '.weather__week').render();
+				return response;
+			} catch (error) {
+				console.error(error);
 			}
-		});
+		};
 
-		console.log(cityData);
-		console.log(weather);
-	};
+		const app = async () => {
+			const weather = await getWeatherDate(crd.latitude, crd.longitude);
 
-	app();
-}
+			const city = await fetch(
+				`https://api.geoapify.com/v1/geocode/reverse?lat=${crd.latitude}&lon=${crd.longitude}&lang=ru&apiKey=5f52dceecbd046de9e150000ef5a2f4e`,
+			);
 
-navigator.geolocation.getCurrentPosition(success, error, options);
+			const cityData = await city.json();
+
+			currentCity.innerHTML = cityData.features[0].properties.city;
+
+			weather.daily.forEach((e, i) => {
+				if (i < 6 && i !== 0) {
+					new DayilyCard(e, '.weather__week').render();
+				}
+			});
+		};
+
+		app();
+	}
+
+	function error(err) {
+		const latitude = 53.89;
+		const longitude = 27.56;
+		console.warn(`ERROR(${err.code}): ${err.message}`);
+
+		const getWeatherDate = async (latitude = 53.89, longitude = 27.56) => {
+			try {
+				const response = await fetch(
+					`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely&appid=11329a68c70484de9078f50717a0d4b7&units=metric&lang=ru`,
+				).then((res) => res.json());
+
+				return response;
+			} catch (error) {
+				console.error(error);
+			}
+		};
+
+		const app = async () => {
+			const weather = await getWeatherDate();
+
+			const city = await fetch(
+				`https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&lang=ru&apiKey=5f52dceecbd046de9e150000ef5a2f4e`,
+			);
+
+			const cityData = await city.json();
+
+			currentCity.innerHTML = cityData.features[0].properties.city;
+
+			weather.daily.forEach((e, i) => {
+				if (i < 6 && i !== 0) {
+					new DayilyCard(e, '.weather__week').render();
+				}
+			});
+		};
+
+		app();
+	}
+
+	navigator.geolocation.getCurrentPosition(success, error, options);
+});
